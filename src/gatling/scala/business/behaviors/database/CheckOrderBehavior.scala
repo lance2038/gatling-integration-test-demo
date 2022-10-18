0459bec284cc55f2cfb97ff64edc2d8d9e3c3506
lance <lance2038@hotmail.com>
2022-10-19 00:52:09 +0800

    BUG fix: if custom checker check failed, exit the workflow

--- src/gatling/scala/business/behaviors/database/CheckOrderBehavior.scala
+++ src/gatling/scala/business/behaviors/database/CheckOrderBehavior.scala
@@ -9,24 +9,19 @@ import io.gatling.core.structure.ChainBuilder
 object CheckOrderBehavior {
 
   val checkOrder: ChainBuilder =
-    exec()
-      .exec(session => {
-        session.set("aiiiiii", "dddddddddddddddd")
-        session
-      })
-      .exec(customSessionChecker("Check Order Status & Extra OrderNo")
-        .executeCheck(session => {
-          var submitOrderSuccess = false
-          val orderDO = OrderSimulationRepository.findById(session("orderId").as[Int])
+    exec(customSessionChecker("Check Order Status & Extra OrderNo")
+      .executeCheck(session => {
+        var submitOrderSuccess = false
+        val orderDO = OrderSimulationRepository.findById(session("orderId").as[Int])
 
-          // store the 'orderNo' to the session, but can not get it below
-          if ("ACTIVE" == orderDO.orderState) {
-            submitOrderSuccess = true
-          }
-          // get orderNo from session, but can not find 'orderNo' in session
-          submitOrderSuccess
-        })
-        .errorMsg("Check Order Status Failed"))
+        // store the 'orderNo' to the session, but can not get it below
+        if ("ACTIVE" == orderDO.orderState) {
+          submitOrderSuccess = true
+        }
+        // get orderNo from session, but can not find 'orderNo' in session
+        submitOrderSuccess
+      })
+      .errorMsg("Check Order Status Failed"))
       .exitHereIfFailed
       .exec(session => {
         // get orderNo from session, but can not find 'orderNo' in session
